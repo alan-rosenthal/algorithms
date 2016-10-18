8cb13da39af7bc68efd2c5a0b909394250c5b1e3
Alan Rosenthal <alan.h.rosenthal@gmail.com>
2016-10-17 22:45:23 -0700

    added pytest class

--- Stack_Class.py
+++ Stack_Class.py
@@ -30,7 +30,7 @@ class Stack(object):
         return self.items[self.size() - 1]
 
     def clear(self):
-        self.items = []
+        self.__init__()
 
     def dump(self):
         if self.size() == 0:
@@ -43,36 +43,55 @@ class Stack(object):
 
 
 #######################################################################
-#   Test Class
+#   PYTEST Class
 #######################################################################
 
 class TestStack(object):
 
-    def test(self):
-
+    def test_basic(self):
         s = Stack()
-
         assert s.isEmpty() == True
         assert s.size() == 0
 
-        for i in range(100):
+    def test_1_item(self):
+        s = Stack()
+        s.push('hello there, world')
+        assert s.isEmpty() == False
+        assert s.size() == 1
+
+        s.clear()
+        assert s.isEmpty() == True
+        assert s.size() == 0
+
+    def test_3(self):
+        s = Stack()
+        s.push('hello')
+        s.push('there')
+        s.push('world')
+        assert s.size() == 3
+        assert s.isEmpty() == False
+
+        assert s.peek() == 'world'
+        item = s.pop()
+        assert item == 'world'
+
+        assert s.peek() == 'there'
+        item = s.pop()
+        assert item == 'there'
+
+        assert s.peek() == 'hello'
+        item = s.pop()
+        assert item == 'hello'
+
+        assert s.size() == 0
+        assert s.isEmpty() == True
+
+    def test_load(self):
+        s = Stack()
+        for i in range(0, 101):
             s.push(i)
-        assert s.peek() == 99
-        assert s.size() == 100
 
-        for i in range(99):
-            s.pop()
-        assert s.pop() == 0
+        for i in range(100, -1, -1):
+            assert s.peek() == i
+            assert s.pop() == i
 
-        assert s.isEmpty() == True
-        assert s.size() == 0
-
-        print('All tests have succeeeded :)')
-        return True
-
-#######################################################################
-#   Main
-#######################################################################
-
-t = TestStack()
-t.test()
